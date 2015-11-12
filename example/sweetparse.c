60d32c9b3305cc57d8dcf729ba2e577d11cc46ae
Adjective-Object <mhuan13@gmail.com>
2015-11-12 15:15:24 -0500

    building the actual library object

---
+++ example/sweetparse.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include "datatypes.h"
+
+unsigned int indentation = 0;
+unsigned int INDENT_SIZE = 2;
+
+void printList(swexp_list_node * node) {
+    while(node != NULL) {
+        switch(node->type){
+            case ATOM:
+                printf("<%s> ", (char *) node->content);
+                break;
+            case LIST:
+                printf("\n");
+                for(int i=0; i<indentation; i++){printf(" ");}
+                printf("(");
+                indentation += INDENT_SIZE;
+                printList(node->content);
+                indentation -= INDENT_SIZE;
+                printf(") ");
+                break;
+            case UNDEFINED:
+                printf("{UNDEFINED}");
+                break;
+        }
+        node = node->next;
+    }
+}
+
+static void print_usage(const char * programname) {
+    printf("%s -i <indent_size> <filenames ...>\n", programname);
+}
+
+static int parse_args(int argc, char ** argv) {
+    // get option arguments into global config variables
+    // return the index of the first non-flag arg
+    char c; 
+    while ((c = getopt(argc, argv, "i:")) != -1) {
+        switch(c) {
+            case 'i':
+                INDENT_SIZE = sscanf("%d", optarg);
+                break;
+            default:
+                printf("unrecognized argument '%c'\n", c);
+                print_usage(argv[0]);
+                exit(1);
+        }
+    }
+    return optind;
+}
+
+int main(int argc, char ** argv) { 
+    FILE * f;
+    int i = parse_args(argc, argv);
+
+    if (i == argc) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    for (; i<argc; i++) {
+        if(!(f = fopen(argv[i], "r"))) {
+            printf("error opening file '%s'\n", argv[i]);
+            exit(1);
+        }
+
+        swexp_list_node * head = parse_file_to_atoms(f);
+        printList(head);
+        printf("\n");
+    }
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,24 +1,51 @@
-program = sweetparse
-srcs = src/sweetparse.c src/datatypes.c
-objs = $(srcs:.c=.o)
+#######################
+# BULDING THE LIBRARY #
+#######################
+
+all: lib example
+
+lib = libsweetexpressions.so
+src = src/datatypes.c
+libobjs = $(src:.c=.o)
 
 LIBS =
-CFLAGS = -g -Wall -std=c99
+CFLAGS = -g -Wall -std=c99 -fPIC
 
+lib: $(lib)
+
+# build the libs from the shared object files
+$(lib): $(libobjs)
+	gcc -shared -o $@ -fPIC $^
+
+src/%.o : src/%.c src/%.h
+	gcc $(CFLAGS) -c $< -o $@
+
+###############################
+# BUILDING THE PARSER EXAMPLE #
+###############################
+
+example_exec = sweetparse
+example_src = example/sweetparse.c
+example_obj = $(example_src:.c=.o)
+example_cflags = -g -Wall -std=c99 -Isrc
+example_ldflags = -L. -lsweetexpressions
+
+example: $(example_exec)
+
+$(example_obj): $(example_src) $(lib)
+	gcc $(example_cflags) -c $^ -o $@
+
+$(example_exec): $(example_obj)
+	gcc $(example_ldflags) $^ -o $@
+
+################
+# MISC UTILITY #
+################
+
+# clean up the example and lib thigs
+clean:
+	rm -f $(libobjs) $(lib) $(example_obj) $(example_exec)
+
+# debug helper to print makefile variables
 print-%:
 	@echo '$*=$($*)'
-
-all: $(program)
-
-$(program): $(objs)
-	g++ $(LDFLAGS) $(objs) $(LIBS) -o $(program)
-
-clean:
-	rm -f $(objs) $(program)
-
-src/$(program).o: src/$(program).c
-	gcc $(CFLAGS) -c $< -o $@
-
-%.o : %.c %.h
-	gcc $(CFLAGS) -c $< -o $@
-
